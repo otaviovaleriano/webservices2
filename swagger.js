dfa01dc954e663e2af9f62058f7a3ee5192cad49
Otavio Silva <otaviovalerianoss@gmail.com>
2024-02-10 05:29:30 -0700

    updating for render

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'My API',
     description: 'Web Services API',
   },
-  host: 'localhost:8080',
-  schemes: ['http'],
+  host: 'webservices2.onrender.com',
+  schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
