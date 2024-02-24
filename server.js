7de5c68cdcd19a01ec9933adc8c888b0a3c8f63e
Otavio Silva <otaviovalerianoss@gmail.com>
2024-02-24 10:18:50 -0700

    starting with oauth okta

--- server.js
+++ server.js
@@ -7,6 +7,31 @@ const mongodb = require('./db/connect');
 // const swaggerUi = require('swagger-ui-express');
 // const swaggerDocument = require('swagger.json');
 
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.CLIENT_SECRET,
+  baseURL: 'http://localhost:8080',
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: 'https://dev-manhaee1d1ymew3x.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+const { requiresAuth } = require('express-openid-connect');
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
