ba7596caa3bd70c2d973d11248a1c52ae4807fc6
Otavio Silva <otaviovalerianoss@gmail.com>
2024-02-27 23:23:42 -0700

    testing protection on route - requiresAuth()

--- routes/cars.js
+++ routes/cars.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
-// const { requiresAuth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
 
 const carsController = require('../controllers/cars');
 const validation = require('../middleware/validate');
 
-router.get('/', carsController.getAllCars);
-router.get('/:id', carsController.getSingleCar);
-router.post('/', validation.saveCar, carsController.createNewCar);
-router.put('/:id', validation.saveCar, carsController.updateCar);
-router.delete('/:id', carsController.deleteCar);
+router.get('/', requiresAuth(), carsController.getAllCars);
+router.get('/:id', requiresAuth(), carsController.getSingleCar);
+router.post('/', requiresAuth(), validation.saveCar, carsController.createNewCar);
+router.put('/:id', requiresAuth(), validation.saveCar, carsController.updateCar);
+router.delete('/:id', requiresAuth(), carsController.deleteCar);
 
 module.exports = router;
